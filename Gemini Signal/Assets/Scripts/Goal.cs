a352d91f33482f2c72bff4d3d20aa353794d5e99
MichaelOSweetman <55377985+MichaelOSweetman@users.noreply.github.com>
2019-10-15 12:37:18 +1100

    Added Goal and Victory Screen

---
+++ Gemini Signal/Assets/Scripts/Goal.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * File Name: Goal.cs
+ * Author: Michael Sweetman
+ * Description: activates the victory screen and disables player movement
+ * Creation Date: 15/10/2019
+ * Last Modified: 15/10/2019
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+	GameObject m_player;
+	GameObject m_victoryScreen;
+	GameObject m_swapBar;
+
+	/*
+	 * Brief: initialisation for the Goal
+	 */
+	private void Start()
+	{
+		// store the player and the UI elements
+		m_player = GameObject.FindGameObjectWithTag("Player");
+		m_victoryScreen = GameObject.FindGameObjectWithTag("VictoryScreen");
+		m_swapBar = GameObject.FindGameObjectWithTag("SwapBar");
+	}
+
+	/*
+	 * Brief: activates victory screen and disables player movement when the player collides with this object
+	 */
+	private void OnCollisionEnter2D(Collision collision)
+	{
+		// if the collision object is the player
+		if (collision.gameObject == m_player)
+		{
+			// disable the player control scripts
+			m_player.GetComponent<PlayerJump>().enabled = false;
+			m_player.GetComponent<PlayerMovement>().enabled = false;
+			m_player.GetComponent<PlayerSwap>().enabled = false;
+
+			// hide the swap bar and show the victory screen
+			m_swapBar.SetActive(false);
+			m_victoryScreen.SetActive(true);
+
+		}
+			print("collided");
+	}
+}
---
+++ Gemini Signal/Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * File Name: VictoryScreen.cs
+ * Author: Michael Sweetman
+ * Description: manages the events triggered by the buttons in the victory screen
+ * Creation Date: 15/10/2019
+ * Last Modified: 15/10/2019
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class VictoryScreen : MonoBehaviour
+{
+	/*
+	 * Brief: Loads the main menu scene
+	 */
+	public void BackToMainMenu()
+	{
+		// load the main menu scene
+		SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
+	}
+
+	/*
+	 * Brief: Quits the game
+	 */
+	public void QuitButton()
+	{
+		// quit the application
+		Application.Quit();
+	}
+}
